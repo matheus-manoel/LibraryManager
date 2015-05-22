f6c1832d5d17b14e7e2d3b4103ebe684ab8237cf
Gabriel <gabrielluizferraz@gmail.com>
2015-05-22 16:39:58 -0300

    getters and setters student and user

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student extends User {
+	private int schoolId;
+
+	void setSchoolId(int schoolId){
+		this.schoolId = schoolId;
+	}
+
+	int getSchoolId(){
+		return this.schoolId;
+	}
+}
--- User.java
+++ User.java
@@ -1,6 +1,40 @@
 public class User {
 	private String name;
-	private String ID;
+	private String id;
 	private String telephoneNumber;
 	private String email;
+
+
+	void setName(String name){
+		this.name = name;
+	}
+
+	String getName(){
+		return this.name;
+	}
+
+	void setID(String id){
+		this.id = id;
+	}
+
+	String getID(){
+		return this.id;
+	}
+
+	void setTelephoneNumber(String telephoneNumber){
+		this.telephoneNumber = telephoneNumber;
+	}
+
+	String getTelephoneNumber(){
+		return this.telephoneNumber;
+	}
+
+	void setEmail(String email){
+		this.email = email;
+	}
+
+	String getEmail(){
+		return this.email;
+	}
+
 }
